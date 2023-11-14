3d21f94ec9277e1935db627bb5aeade307b047b9
Sean Lee <sean@vessl.ai>
2023-11-14 20:34:21 +0900

    Add variables

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "node_selectors" {
+  type = list(object({
+    key   = string
+    value = string
+  }))
+  default = [{
+    key   = "v1.k8s.vessl.ai/dedicated"
+    value = "manager"
+  }]
+}
+
+variable "tolerations" {
+  type = list(object({
+    key      = string
+    operator = string
+    value    = optional(string)
+    effect   = optional(string)
+  }))
+  default = [{
+    key      = "v1.k8s.vessl.ai/dedicated"
+    operator = "Exists"
+    effect   = "NoSchedule"
+  }]
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    "vessl:managed" : "true",
+  }
+}
