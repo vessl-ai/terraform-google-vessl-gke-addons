090a479d41f7a0f47d67c2b5fb6a73675b7303a5
Sean Lee <sean@vessl.ai>
2024-02-27 23:09:34 +0900

    Add ingress-nginx

---
+++ ingress_nginx.tf
@@ -0,0 +1,38 @@
+locals {
+  // https://github.com/kubernetes/ingress-nginx/blob/main/charts/ingress-nginx/values.yaml
+  ingress_nginx_helm_values = {
+    controller = {
+      service = {
+        targetPorts = {
+          http  = "http",
+          https = try(var.ingress_nginx.ssl_termination, false) ? "http" : "https",
+        },
+        annotations = var.ingress_nginx.service_annotations,
+      }
+      admissionWebhooks = {
+        patch = {
+          tolerations = local.tolerations
+          nodeSelector = merge(
+            { for expression in var.node_affinity : expression.key => expression.values[0] },
+            { "kubernetes.io/os" : "linux" },
+          )
+        }
+      }
+      affinity = {
+        nodeAffinity = local.node_affinity
+      }
+      tolerations = local.tolerations
+    }
+  }
+}
+
+resource "helm_release" "ingress_nginx" {
+  count = var.ingress_nginx != null ? 1 : 0
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  name       = "ingress-nginx"
+  version    = var.ingress_nginx.version
+  namespace  = var.ingress_nginx.namespace
+  values     = merge(local.ingress_nginx_helm_values, var.ingress_nginx.extra_chart_values)
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,27 @@
+variable "ingress_nginx" {
+  type = object({
+    namespace           = optional(string, "kube-system")
+    version             = optional(string, "4.9.1")
+    service_annotations = optional(map(string), {})
+    ssl_termination     = optional(bool, true)
+    extra_chart_values  = optional(map(string), {})
+  })
+  default = null
+}
+
+variable "node_affinity" {
+  type = list(object({
+    key      = string
+    operator = string
+    values   = optional(list(string))
+  }))
+  default = [{
+    key      = "v1.k8s.vessl.ai/dedicated"
+    operator = "In"
+    values   = ["manager"]
+  }]
+}
+
 variable "node_selectors" {
   type = list(object({
     key   = string
